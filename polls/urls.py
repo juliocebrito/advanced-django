40161e3f7859a7806bb89a420229adb2c8f15fd8
Julio Brito <jucebridu@gmail.com>
2022-04-26 19:07:34 -0500

    first commit

--- polls/urls.py
+++ polls/urls.py
@@ -7,10 +7,18 @@ DetailQuestionView, UpdateQuestionView, DeleteQuestionView)
 app_name = 'polls'
 
 urlpatterns = [
-    path('create_example_model', views.create_example_model, name='create_example_model'),
-    # path('', views.polls, name='polls'),
-    path('list_question/', ListQuestionView.as_view(), name='list_question'),
+    # CRUD VIEWS (CREATE, READ[LIST, DETAIL], UPDATE, DELETE)
+
+    path('create_example_model/', views.create_example_model, name='create_example_model'),
+    path('list_example_model/', views.list_example_model, name='list_example_model'),
+    path('detail_example_model/<int:pk>/', views.detail_example_model, name='detail_example_model'),
+    path('update_example_model/<int:pk>/', views.update_example_model, name='update_example_model'),
+    path('delete_example_model/<int:pk>/', views.delete_example_model, name='delete_example_model'),
+
+    # CRUD CLASS-BASED VIEWS (CREATE, READ[LIST, DETAIL], UPDATE, DELETE)
+    
     path('create_question/', CreateQuestionView.as_view(), name='create_question'),
+    path('list_question/', ListQuestionView.as_view(), name='list_question'),
     path('detail_question/<int:pk>/', DetailQuestionView.as_view(), name='detail_question'),
     path('update_question/<int:pk>/', UpdateQuestionView.as_view(), name='update_question'),
     path('delete_question/<int:pk>/', DeleteQuestionView.as_view(), name='delete_question'),
--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .models import ExampleModel, Question
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-
+from .models import ExampleModel, Question
 from .forms import ExampleModelForm, QuestionForm
 
 
+# CRUD VIEWS (CREATE, READ[LIST, DETAIL], UPDATE, DELETE)
+
+@login_required
 def create_example_model(request):
     if request.method == 'POST':
         form = ExampleModelForm(request.POST)
@@ -20,7 +23,31 @@ def create_example_model(request):
         form = ExampleModelForm()
     return render(request, 'create_example_model.html', {'form': form})
 
+@login_required
+def list_example_model(request):
+    pass
 
+@login_required
+def detail_example_model(request):
+    pass
+
+@login_required
+def update_example_model(request):
+    pass
+
+@login_required
+def delete_example_model(request):
+    pass
+
+
+# CRUD CLASS-BASED VIEWS (CREATE, READ[LIST, DETAIL], UPDATE, DELETE)
+
+class CreateQuestionView(LoginRequiredMixin, CreateView):
+    login_url = 'users:home'
+    form_class = QuestionForm
+    template_name = 'polls/create_question.html'
+    success_url = reverse_lazy('polls:list_question')
+    
 
 class ListQuestionView(LoginRequiredMixin, ListView):
     login_url = 'users:home'
@@ -35,17 +62,10 @@ class ListQuestionView(LoginRequiredMixin, ListView):
         return context
 
 
-class CreateQuestionView(LoginRequiredMixin, CreateView):
-    login_url = 'users:home'
-    form_class = QuestionForm
-    template_name = 'polls/create_question.html'
-    success_url = reverse_lazy('polls:list_question')
-
 class DetailQuestionView(LoginRequiredMixin, DetailView):
     login_url= 'users:home'
     model = Question
     template_name = 'polls/detail_question.html'
-
 
 
 class UpdateQuestionView(LoginRequiredMixin, UpdateView):
@@ -54,6 +74,7 @@ class UpdateQuestionView(LoginRequiredMixin, UpdateView):
     form_class = QuestionForm
     template_name = 'polls/update_question.html'
     success_url = reverse_lazy('polls:list_question')
+    
 
 class DeleteQuestionView(LoginRequiredMixin, DeleteView):
     login_url = 'users:home'
